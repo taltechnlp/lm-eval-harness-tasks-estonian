a3a13944c7a2d63f233faf6d6f64074e8f9f229a
helenl <helenl@base.intra.ttu.ee>
2025-05-20 11:24:05 +0300

    Paevakaja base version now also calculates precision and recall

--- tasks/paevakaja_et/utils.py
+++ tasks/paevakaja_et/utils.py
@@ -15,3 +15,58 @@ def clean_text(text: str) -> str:
     text = text.lower()
 
     return text
+
+def speakers(items):
+    print(items)
+    return items
+
+def speakers_agg(items):
+    goodness = 0
+    print(items)
+    for item in items:
+        print([item[0]])
+        print([item[1]])
+        print("---------------------")
+    return 0.78
+
+def process_results_speakers(a, b):
+    extracted = []
+    cache = ""
+    state = "out"
+    string_char = None
+    for ch in b[0]:
+        if state == "out":
+            if ch == "[":
+                state = "in"
+            continue
+        if state == "in":
+            if ch == '"' or ch == "'":
+                state = "in_string"
+                string_char = ch
+            elif ch == "]":
+                state = "out"
+                break
+            continue
+        if state == "in_string":
+            if ch == string_char:
+                extracted.append(cache)
+                cache = ""
+                state = "in"
+            else:
+                cache += ch
+    if cache:
+        extracted.append(cache)
+    print("Model output:")
+    print(b)
+    print("Extracted list:")
+    print(extracted)
+    print("Expected output:")
+    print(a["speakers"])
+    print("-----------------------------------")
+    correct = list(set(a["speakers"]))
+    guesses = list(set(extracted))
+    tp_count = len(set(correct).intersection(set(guesses)))
+    return {
+        "precision": tp_count / max(len(guesses), 1),
+        "recall": tp_count / max(len(correct), 1),
+    }
--- tasks/paevakaja_et_instr/utils.py
+++ tasks/paevakaja_et_instr/utils.py
@@ -1,5 +1,4 @@
 import string
-
 import evaluate
 
 
